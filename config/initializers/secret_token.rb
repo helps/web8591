befc5e327cbaf7530bd022212e3f5c703a7d6862
bobo <me@bobo.im>
2013-03-06 13:39:39 +0800

    fisrt commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Web8591::Application.config.secret_key_base = 'e05c034402d2aa3abb82cb8a607ecfce0af0d3cf97025e324dfc825410a0f10cd503f00a0e6abb7f81570700b3267da1d146660f0d085829a0d5d697ebca3051'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Web8591::Application.config.session_store :encrypted_cookie_store, key: '_web8591_session'
